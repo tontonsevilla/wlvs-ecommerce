fffc21879e6ec7ea66f2b5f1afb965b14f44a8c5
Winston Sevilla <wsevilla@blastasia.com>
2026-02-28 15:21:00 +0800

    update: Add EntityFramework

---
+++ WLVSEcommerce/WLVSEcommerce.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WLVSEcommerce.Web.Models;
+
+namespace WLVSEcommerce.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ WLVSEcommerce/WLVSEcommerce.Web/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WLVSEcommerce.Web.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
